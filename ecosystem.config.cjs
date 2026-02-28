14908a4c84d3ad8cb1125940c35af8773b825c81
Michael <382968311@qq.com>
2026-02-28 17:34:48 +0800

    updated

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,26 @@
 module.exports = {
   apps: [{
     name: 'city-roulette',
-    script: 'npx',
-    args: 'serve dist -p 3000',
+
+    // script: 'npx',
+    // args: 'serve dist -p 3000',
+    // env: {
+    //   NODE_ENV: 'production'
+    // },
+
+    script: 'npm',
+    args: 'run dev',
+    env: {
+      NODE_ENV: 'development'
+    },
+
     cwd: process.cwd(),
     instances: 1,
     exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    },
+
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
